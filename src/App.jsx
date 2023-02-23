92b2faa8dcdffe205c854e7a5cbd951f5f427b21
alexrcenteno <alex.centeno1991@gmail.com>
2023-02-23 13:04:12 -0600

    moves individual sections into their own components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+    </header>
+  );
 }
 
-export default App
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <form>
+        <div>
+          Title: <input type="text" />
+        </div>
+        <div>
+          Author: <input type="text" />
+        </div>
+        <button type="submit">Create Post</button>
+      </form>
+    </div>
+  );
+}
+
+function PostsIndex() {
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      <div className="posts">
+        <h2>Code For Dummies</h2>
+        <img src="https://pictures.abebooks.com/isbn/9781118951309-us.jpg" alt="" />
+        <p>Author: Alex Centeno</p>
+        <button>More Info</button>
+      </div>
+      <div className="Posts">
+        <h2>NFL Draft</h2>
+        <img src="https://pbs.twimg.com/profile_images/1611754721701773314/JKLJNXKU_400x400.jpg" alt="" />
+        <p>Author: Jake Smith</p>
+        <button>More Info</button>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 20XX</p>
+    </footer>
+  );
+}
+
+function Content() {
+  return (
+    <div>
+      <PostsNew />
+      <PostsIndex />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
