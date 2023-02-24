7f70c4f36efc1d3053bde9ea5fe9927102214266
alexrcenteno <alex.centeno1991@gmail.com>
2023-02-24 12:29:45 -0600

    adds static modal

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
+import { Modal } from "./Modal";
 
 export function Content() {
   const [posts, setPosts] = useState([]);
@@ -19,6 +20,9 @@ export function Content() {
     <div>
       <PostsNew />
       <PostsIndex posts={posts} />
+      <Modal show={true}>
+        <p>TEST</p>
+      </Modal>
     </div>
   );
 }
